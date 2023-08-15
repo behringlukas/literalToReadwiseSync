d952ebad293357e1823af1eaebff8f50322155f1
behringlukas <lukasbehringer.lb11@googlemail.com>
2023-08-16 01:38:53 +0200

    general fixes and improvements

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
     ],
   },
   plugins: [
